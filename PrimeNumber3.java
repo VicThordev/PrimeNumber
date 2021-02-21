ad126944bad199fd115dec3fbf4c3209c6cc6670
VicThordev <78712239+VicThordev@users.noreply.github.com>
2021-02-21 15:59:06 +0100

    Add files via upload
    Assignment2

--- PrimeNumber3.java
+++ PrimeNumber3.java
@@ -1,33 +1,34 @@
-import java.text.MessageFormat;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PrimeNumber3 {
-    int vic=0; int pNum2=0;
-    boolean ade=false;
-    Scanner scn = new Scanner(System.in);
-    public int  vic1() {
-        System.out.println("Enter your input: ");
-        int pNum;
-        try (Scanner sca = new Scanner(System.in)) {
-            for (int i = 0; i < 10; i++) {
-                pNum = sca.nextInt();
-                if (pNum <= 1 || pNum % 2 == 0)
-                    System.out.println(MessageFormat.format("{0} is not a prime number.", pNum));
-                    for (int j = 2; j < Math.sqrt(pNum); j++) {
-                        if (pNum % j != 0) {
-                            System.out.println(pNum + " is a prime number.");
-                            vic+=pNum;
-                        }
-                    }
-                    System.out.printf("%d is the sum of prime numbers.%n", vic);
-                }
+    int pNumAdd=0; int pNum2=0;
+    ArrayList<Integer> arr =new ArrayList<>(10);
+    public void primeMethod1(ArrayList<Integer> arr) {
+        arr.add(1);
+        arr.add(3);
+        arr.add(13);
+        arr.add(21);
+        arr.add(24);
+        arr.add(45);
+        arr.add(17);
+        for (int pNum:arr) {
+            for (int i = 2; i < Math.sqrt(pNum); i*=2) {
+                if (pNum % i == 0||pNum<2||pNum%2==0) System.out.println(pNum+" is not a prime number.");
+                else primeMethod2(pNum);
             }
-            return vic;
         }
+
+        }
+
+    private void primeMethod2(int pNum) {
+        System.out.println(pNum + " is a prime number.");
+        pNumAdd+= pNum;
+        System.out.printf("%d is the sum of prime numbers.%n", pNumAdd);
+    }
 
 
     public static void main(String [] args) {
         PrimeNumber3 primeNumber3 = new PrimeNumber3();
-        primeNumber3.vic1();
+        primeNumber3.primeMethod1(primeNumber3.arr);
     }
 }
